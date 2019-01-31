2414111621548770e297e3d791e121ff4763826c
Sam Schott <ss2151@cam.ac.uk>
2019-01-31 23:12:41 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 import mock
 
-MOCK_MODULES = ['PyQt5']
+MOCK_MODULES = ['qtpy']
 sys.modules.update((mod_name, mock.MagicMock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('.'))
