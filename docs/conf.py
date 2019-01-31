54c8c661e347afd1a8f8ea5c50d8bb4c8fa5fa88
Sam Schott <ss2151@cam.ac.uk>
2019-01-31 23:24:59 +0000

    doc updates

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,8 @@ import os
 import sys
 import mock
 
-MOCK_MODULES = ['qtpy']
-sys.modules.update((mod_name, mock.MagicMock()) for mod_name in MOCK_MODULES)
+autodoc_mock_imports = ['sip', 'PyQt5', 'PyQt5.QtGui', 'PyQt5.QtCore', 'PyQt5.QtWidgets']
+
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
