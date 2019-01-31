2aebaf2da820e333892ff9761738284640935d87
Sam Schott <ss2151@cam.ac.uk>
2019-01-31 18:41:31 +0000

    updated doc

--- source/conf.py
+++ source/conf.py
@@ -98,7 +98,6 @@ html_logo = 'resources/logo.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme_path = ['_theme']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
