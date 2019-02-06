d580199ad47976d229e6be80eef2a43ba1f3db53
Sam Schott <ss2151@cam.ac.uk>
2019-02-06 23:32:33 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 import time
 
-autodoc_mock_imports = ['PySide', 'PySide.QtGui', 'PySide.QtCore', 'PySide.QtWidgets']
+autodoc_mock_imports = ['qtpy']
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../customxepr'))
