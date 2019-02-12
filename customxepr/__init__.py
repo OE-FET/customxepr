f23b239c3ef79e5b8708d9bcea1a010f5971a5ed
Sam Schott <ss2151@cam.ac.uk>
2019-02-12 13:31:49 +0000

    Python 2 compatibility fixes

--- customxepr/__init__.py
+++ customxepr/__init__.py
@@ -1,7 +1,8 @@
-from __future__ import absolute_import
+from __future__ import division, absolute_import, unicode_literals
 import os
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if not on_rtd:
     from customxepr.startup import run
 from customxepr.main import CustomXepr
+from customxepr.xepr_dataset import XeprData, XeprParam
 from customxepr.manager import queued_exec
