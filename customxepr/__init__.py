ccd0d06f35948efc18bc579927e8fedc730520b5
Sam Schott <ss2151@cam.ac.uk>
2019-03-08 18:21:51 +0000

    bug fixes

--- customxepr/__init__.py
+++ customxepr/__init__.py
@@ -6,7 +6,7 @@ from customxepr.xepr_dataset import XeprData, XeprParam
 from customxepr.manager import queued_exec
 
 # =======================================================================================
-# work around some pyqtgraph at Qt bugs
+# suppress some Qt error messages
 # =======================================================================================
 
 from qtpy import QtCore
@@ -17,22 +17,8 @@ def handler(msg_type, msg_log_context, msg_string):
 
 QtCore.qInstallMessageHandler(handler)
 
-def cleanup():
-    global _cleanupCalled
-    if _cleanupCalled:
-        return
-
-    if not getConfigOption('exitCleanup'):
-        return
-
-    ViewBox.quit()  ## tell ViewBox that it doesn't need to deregister views anymore.
-    _cleanupCalled = True
-
 try:
     # import pyqtgraph before creating QApplication to avoid bugs
     import pyqtgraph
-    # monkeypatch pyqtgraph cleanup to avoid crash in exit from garbage collection
-    pyqtgraph.cleanup = cleanup
-
 except ImportError:
     pass
