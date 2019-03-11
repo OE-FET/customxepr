a570e447fb3dfdcb01b76925448f098261676a4e
SamSchott <ss2151@cam.ac.uk>
2019-03-11 17:35:03 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ release = get_metadata('../customxepr/main.py', '__version__')
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = '1.8.4'
+needs_sphinx = '1.7.5'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
