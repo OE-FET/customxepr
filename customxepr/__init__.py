75e1c50361f46914101e3f6f8d85ee3104979e87
Sam Schott <ss2151@cam.ac.uk>
2019-03-28 14:23:41 +0000

    moved QT related initialization to gui/__init__

--- customxepr/__init__.py
+++ customxepr/__init__.py
@@ -4,21 +4,3 @@ from customxepr.main import CustomXepr
 from customxepr.manager import Experiment, Manager, queued_exec
 from customxepr.experiment.xepr_dataset import XeprData, XeprParam
 from customxepr.experiment.mode_picture_dataset import ModePicture
-
-# =======================================================================================
-# suppress some Qt error messages
-# =======================================================================================
-
-from qtpy import QtCore
-
-
-def handler(msg_type, msg_log_context, msg_string):
-    pass
-
-QtCore.qInstallMessageHandler(handler)
-
-try:
-    # import pyqtgraph before creating QApplication to avoid bugs
-    import pyqtgraph
-except Exception:
-    pass
--- customxepr/gui/__init__.py
+++ customxepr/gui/__init__.py
@@ -2,3 +2,21 @@
 from .manager_window import ManagerApp
 from .customxepr_window import CustomXeprGuiApp
 from .splash import SplashScreen
+
+# =======================================================================================
+# suppress some Qt error messages
+# =======================================================================================
+
+from qtpy import QtCore
+
+
+def handler(msg_type, msg_log_context, msg_string):
+    pass
+
+QtCore.qInstallMessageHandler(handler)
+
+try:
+    # import pyqtgraph before creating QApplication to avoid bugs
+    import pyqtgraph
+except Exception:
+    pass
