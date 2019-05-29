3c85231c5fcee9812e4c75b6880e63744ea42392
Sam Schott <ss2151@cam.ac.uk>
2019-05-29 10:27:05 +0100

    update XeprData test, clean up files

--- tests/experiment/tests_xepr_dataset.py
+++ tests/experiment/tests_xepr_dataset.py
@@ -28,9 +28,9 @@ class TestXeprData(unittest.TestCase):
 
     def test_load_save_2d(self):
         """
-        Test loading and saving BES3T data files form Xepr. Assert that the saved files
-        have the same content as the original files. This test will fail in Python 2 since
-        the order of sections will not be maintained.
+        Test loading and saving BES3T 2D data files. Assert that the saved files have the
+        same content as the original files. This test will fail in Python 2 since the
+        order of sections will not be maintained.
         """
 
         # test loading and saving in the correct file format
@@ -43,11 +43,28 @@ class TestXeprData(unittest.TestCase):
                 self.assertTrue(filecmp.cmp(self.PATH_ORIGINAL_2D + ext,
                                             self.PATH_NEW_2D + ext))
 
+    def test_modify_ordinate(self):
+        """
+        Tests getter and setter methods for ordinate data by verifying that the data does
+        not change in a get / set cycle.
+        """
+
+        # test loading and saving in the correct file format
+
+        dset = XeprData(self.PATH_ORIGINAL_2D + '.DSC')
+        dset.o = dset.o  # should not change the actual content
+        dset.save(self.PATH_NEW_2D + '.DSC')
+
+        for ext in EXTENTIONS:
+            if os.path.isfile(self.PATH_ORIGINAL_2D + ext):
+                self.assertTrue(filecmp.cmp(self.PATH_ORIGINAL_2D + ext,
+                                            self.PATH_NEW_2D + ext))
+
     def test_load_save_complex(self):
         """
-        Test loading and saving BES3T data files form Xepr. Assert that the saved files
-        have the same content as the original files. This test will fail in Python 2 since
-        the order of sections will not be maintained.
+        Test loading and saving BES3T data files with complex data. Assert that the saved
+        files have the same content as the original files. This test will fail in Python 2
+        since the order of sections will not be maintained.
         """
 
         # test loading and saving in the correct file format
@@ -93,6 +110,12 @@ class TestXeprData(unittest.TestCase):
         self.assertEqual(dset.dsl.groups['customXepr'].pars['NewParam1'].value, 1234)
         self.assertEqual(dset.dsl.groups['customXepr'].pars['NewParam2'].value, 1234)
 
+    def tearDown(self):
+
+        new_files = [os.path.join(DIR, f) for f in os.listdir(DIR) if 'new' in f]
+
+        for f in new_files:
+            os.remove(f)
 
 if __name__ == '__main__':
     unittest.main()
