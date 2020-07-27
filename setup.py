29f18e10adee92958070b5726b05ab953d970e05
Sam Schott <ss2151@cam.ac.uk>
2020-07-27 21:05:55 +0200

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,71 +1,52 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-
-def get_metadata(relpath, varname):
-    """Read metadata info from a file without importing it."""
-    from os.path import dirname, join
-
-    if "__file__" not in globals():
-        root = "."
-    else:
-        root = dirname(__file__)
-
-    for line in open(join(root, relpath), "rb"):
-        line = line.decode("cp437")
-        if varname in line:
-            if '"' in line:
-                return line.split('"')[1]
-            elif "'" in line:
-                return line.split("'")[1]
-
-
 setup(
-    name="customxepr",
-    version=get_metadata("customxepr/main.py", "__version__"),
-    description="Python interface for for Bruker Xepr.",
-    url=get_metadata("customxepr/main.py", "__url__"),
-    author="Sam Schott",
-    author_email="ss2151@cam.ac.uk",
-    license="MIT",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    name='customxepr',
+    version='',
+    description='Python interface for for Bruker Xepr.',
+    url='v3.1.1',
+    author='Sam Schott',
+    author_email='ss2151@cam.ac.uk',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     package_data={
-            "customxepr": [
-                    "gui/resources/*.icns",
-                    "gui/resources/*.png",
-                    "gui/resources/*.md",
-                    "gui/resources/*.ui",
-                    "gui/*.ui",
-                    "experiment/*.txt",
+            'customxepr': [
+                    'gui/resources/*.icns',
+                    'gui/resources/*.png',
+                    'gui/resources/*.md',
+                    'gui/resources/*.ui',
+                    'gui/*.ui',
+                    'experiment/*.txt',
             ],
     },
     install_requires=[
-                "decorator",
-                "future",
-                "ipython",
-                "keithley2600>=1.2.1",
-                "keithleygui>=1.1.2",
-                "pint",
-                "pyvisa",
-                "pyvisa-py",
-                "lmfit",
-                "matplotlib",
-                "markdown2",
-                "numpy",
-                "pygments",
-                "PySignal",
-                "PyQt5",
-                "qtconsole",
-                "configparser;python_version<='2.7'",
-                "scipy",
-                "mercuryitc>=0.2.1",
-                "mercurygui>=1.1.2",
+                'decorator',
+                'future',
+                'ipython',
+                'keithley2600>=1.2.1',
+                'keithleygui>=1.1.2',
+                'pint',
+                'pyvisa',
+                'pyvisa-py',
+                'lmfit',
+                'matplotlib',
+                'markdown2',
+                'numpy',
+                'pygments',
+                'PySignal',
+                'PyQt5',
+                'qtconsole',
+                'configparser;python_version<="2.7"',
+                'scipy',
+                'mercuryitc>=0.2.1',
+                'mercurygui>=2.0.0',
     ],
     zip_safe=False,
     entry_points={
-      "console_scripts": ["customxepr=customxepr.startup:run"],
+      'console_scripts': ['customxepr=customxepr.startup:run'],
       },
     classifiers=[
         'License :: OSI Approved :: MIT License',
