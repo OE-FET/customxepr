f5d85ac9d2f5b873937d1eaa8f26614f43d909f2
Sam Schott <ss2151@cam.ac.uk>
2021-11-01 14:29:06 +0000

    [docs] migrate m2r -> m2r2

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
-    "m2r",
+    "m2r2",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
